b2d069a4fc1b9fee595c1db731457fad24d43682
dark567 <darkmit@list.ru>
2019-09-13 11:05:34 +0300

    menuItemsValidation

--- BugTrackingSystem/Console/Program.cs
+++ BugTrackingSystem/Console/Program.cs
@@ -32,42 +32,29 @@ namespace Console
                     case 1:
                         System.Console.WriteLine("\t Insert the Task you want to add:");
 
-                        try
-                        {
-                            //void AddTask
-                            System.Console.WriteLine("\t Task added successfully.");
-                        }
-                        catch (Exception e)
-                        {
-                            System.Console.WriteLine("\tError. Something went wrong!)", e.Message);
-                        }
-
+                        AddTask();
+                       //HistoryTaskAdd();
                         ShowMenuInConsole();
+
                         break;
                     case 2:
 
-                        try
-                        {
-                            //void ChangedTask
-                            System.Console.WriteLine("\t Task changed successfully.");
-                        }
-                        catch (Exception e)
-                        {
-                            System.Console.WriteLine("\tError. Something went wrong!)", e.Message);
-                        }
-
+                        ChangeTask();
+                        //HistoryTaskAdd();
                         ShowMenuInConsole();
+
                         break;
                     case 3:
+
                         //void Simulation
                         ShowMenuInConsole();
+
                         break;
                     case 4:
 
                         try
                         {
                             //void ReadHistoryTasks
-                            System.Console.WriteLine("\t Task changed successfully.");
                         }
                         catch (Exception e)
                         {
@@ -81,7 +68,7 @@ namespace Console
                         try
                         {
                             //void ClearConsole
-                            System.Console.WriteLine("\t Task changed successfully.");
+                            System.Console.WriteLine("\t Tasks clear successfully.");
                         }
                         catch (Exception e)
                         {
@@ -101,6 +88,85 @@ namespace Console
             }
         }
 
+        private static void ChangeTask()
+        {
+            int _nomerTask;
+            int index = 0;
+            do
+            {
+                System.Console.Write("\t Write Task №:");
+            } while (!Int32.TryParse(System.Console.ReadLine(), out _nomerTask));
+
+
+            //System.Console.WriteLine($"[index] \t Id \t Name \t Prioritet \t ComplexityTask \t Type Task");
+            ////show tasks
+            //foreach (SomeTask task in SomeTaskLogicLayer.GetTasks)
+            //{
+            //    System.Console.WriteLine($"[{index++}] \t {task.Id} \t {task.Name} \t {task.Prioritet} \t {task.ComplexityTask} \t {task.TypeTask}");
+            //}
+
+            //int index = SomeTask.FindIndex(c => c.Name == SomeVariable);
+            AddTask(_nomerTask);
+        }
+
+        /// <summary>
+        /// Add Task
+        /// </summary>
+        private static void AddTask(int nomerTask = 0)
+        {
+            try
+            {
+                string NameTask;
+                int TypeTask, ComplexityTask;
+                bool pr = false;
+
+                //Name
+                System.Console.Write("\t Write Name Task:");
+                NameTask = System.Console.ReadLine();
+
+                //Complexity
+                do
+                {
+                    System.Console.Write("\t Write Complexity Task(from 1 to 5):");
+                    pr = int.TryParse(System.Console.ReadLine(), out ComplexityTask);
+                    if (!(ComplexityTask >= 1 && ComplexityTask <= 5))
+                    {
+                        System.Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.Write("\t Write Complexity only from 1 to 5\tInvalid input. Try again:\n");
+                        System.Console.ResetColor();
+                        pr = false;
+                    }
+                } while (!pr);
+
+                //Type
+                do
+                {
+                    System.Console.Write("\t Write Type Task(Bag:1, SomeTask:2, Technical debt: 3):");
+                    pr = int.TryParse(System.Console.ReadLine(), out TypeTask);
+                    if (!(TypeTask >= 1 && TypeTask <= 3))
+                    {
+                        System.Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.Write("\t Write Type only 1 or 2 or 3\tInvalid input. Try again:\n");
+                        System.Console.ResetColor();
+                        pr = false;
+                    }
+                } while (!pr);
+
+                //if (nomerTask = 0)  ListTask.AddTask(new SomeTask(name: NameTask, complexity: ComplexityTask, typeTask: TypeTask));
+                //else ListTask.ChangeTask(new SomeTask(name: NameTask, complexity: ComplexityTask, typeTask: TypeTask));
+
+                System.Console.WriteLine("\tSuccessfully Task Add");
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("\tError. Something went wrong!)", e.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
         private static void ShowMenuInConsole()
         {
             System.Console.WriteLine("\n Please choose one of the options:");
