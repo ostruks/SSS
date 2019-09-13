37b84511499b6a229ab0086ca9e5c8680ee32de4
Serhii Ostruk <sero@ciklum.com>
2019-09-13 09:02:55 +0300

    test

---
+++ BugTrackingSystem/Library/BaseTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public abstract class BaseTask
+    {
+        protected string _name;
+        protected int _priority;
+        public int Complexity { get; set; }
+        public BaseTask(string Name, int Priority)
+        {
+            _name = Name;
+            _priority = Priority;
+        }
+    }
+}
