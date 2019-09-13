78cabc72a2a8e4f1c9f8be760d88667958e0a575
dark567 <darkmit@list.ru>
2019-09-13 13:47:15 +0300

    Add TechnicalDebt Class

---
+++ BugTrackingSystem/Library/Tasks/TechnicalDebtLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tasks
+{
+    public class TechnicalDebtLogic
+    {
+        public static TechnicalDebt[] GetTechnicalDebts
+        {
+            get => TechnicalDebt._technicalDebtList.ToArray();
+        }
+
+        public static void AddTechnicalDebt(TechnicalDebt _technicalDebt)
+        {
+            TechnicalDebt._technicalDebtList.Add(_technicalDebt);
+        }
+
+        public static void DelItemTechnicalDebt(int index)
+        {
+            TechnicalDebt._technicalDebtList.RemoveAt(index);
+        }
+    }
+}
