23e5235dfb0f3d5c84cc71947e193c65338b451f
Serhii <sboyko1995@gmail.com>
2019-09-14 14:22:25 +0300

    Added Bug class

---
+++ BugTrackingSystem/Library/Tasks/Bug.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tasks
+{
+    public class Bug : BaseTask
+    {
+        public static List<Bug> _bugList;
+
+        static Bug()
+        {
+            _bugList = new List<Bug>();
+        }
+
+        public Bug(string Name, int Priority, int Complexity) : base(Name, Priority, Complexity)
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
