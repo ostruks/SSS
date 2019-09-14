4ad5fb3625267a4866f2bac65530e607dabe6557
Serhii <sboyko1995@gmail.com>
2019-09-14 15:29:41 +0300

    Added class BugLogic

---
+++ BugTrackingSystem/Library/Tasks/BugLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tasks
+{
+    public class BugLogic
+    {
+        public static Bug[] GetBug
+        {
+            get => Bug._bugList.ToArray();
+        }
+
+        public static void AddBug(Bug _bug)
+        {
+            Bug._bugList.Add(_bug);
+        }
+
+        public static void DelItemBug(int index)
+        {
+            Bug._bugList.RemoveAt(index);
+        }
+    }
+}
